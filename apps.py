ee82dc02c95a193465615ee9f8a632b33df2d804
Xinyi Yang <101031528+Cynthia-Young@users.noreply.github.com>
2024-06-24 16:37:23 +0800

    Update apps.py

--- apps.py
+++ apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BackmanageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BackManage'
+    name = 'BackManage2111111111456f8ui'
